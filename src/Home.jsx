b3171493ab3cf8a87ec8d042efbb4e40733622ba
AlzaHendrian <alzahendriaan@gmail.com>
2023-08-05 23:00:29 +0700

    rubah component home

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,14 +11,13 @@ const Home = () => {
             <button onClick={() => navigate('#')} className="bg-blue-400 rounded-xl text-white me-3 ms-3 p-4" style={{ boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)' }}>D.ERD_TEST</button>
         </div>
         <div className="bg-[#F6F4EB] p-5 mt-10 text-center ms-5 me-5 rounded-lg" style={{ boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)' }}>
-            <div className="grid grid-cols-2">
-                <h1>Soal sql test jawaban ada di dalam folder my-project</h1>
-                <h1>Soal E.ERD_TEST jawaban ada di dalam folder src</h1>
-            </div>
-            <div className="grid grid-cols-2">
+                <h1>Soal A.CSS TEST ada di dalam folder src</h1>
+                <h1>Soal B.API TEST ada di dalam folder src</h1>
+                <h1>Soal C.SQL TEST jawaban ada di dalam folder my-project</h1>
+                <h1>Soal D.ERD TEST belum dikerjakan</h1>
+                <h1>Soal E.ERD TEST jawaban ada di dalam folder src</h1>
                 <h1>Soal F.Penjelasan jawaban ada di dalam folder my-project</h1>
                 <h1>Soal G.Penjelasan jawaban ada di dalam folder my-project</h1>
-            </div>
         </div>
         </>
     )
