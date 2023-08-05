cda37fb1dfc9ebfa3a18f69ec1d06716f9a113f6
AlzaHendrian <alzahendriaan@gmail.com>
2023-08-05 22:37:29 +0700

    tambahan ekstensi pada fake data

--- src/B.API_TEST/components/CardJob.jsx
+++ src/B.API_TEST/components/CardJob.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { data } from '../../fakedata/fakedata';
+import { data } from '../../fakedata/fakedata.js';
 import JobDetail from '../pages/JobDetail';
 
 const CardJob = () => {
